73f735c2d8ddc13218e85c29bc9e49b7f70fb13d
Naoh <27915369+N40h@users.noreply.github.com>
2023-08-18 18:11:57 +0200

    Added filtering function on eventlistener

--- FrontEnd/assets/js/gallery.js
+++ FrontEnd/assets/js/gallery.js
@@ -2,6 +2,14 @@
 /*         DOM
 /**********************/
 const gallery = document.querySelector('.gallery');
+const filters = document.querySelector('.filters');
+
+/***********************
+/*      Variables
+/**********************/
+let works = [];
+let categories = [];
+let displayedWorks = [];
 
 /***********************
 /*      Functions
@@ -9,23 +17,62 @@ const gallery = document.querySelector('.gallery');
 async function getWorks() {
 	const response = await fetch('http://localhost:5678/api/works');
 	const data = await response.json();
-	console.log(data);
-	display(data);
+
+	works = data;
+	displayedWorks = works;
+
+	displayWorks(displayedWorks);
 }
-getWorks();
 
-function display(data) {
-	for (let i = 0; i < data.length; i++) {
-		const galleryItem = document.createElement('figure');
-		const galleryItemImage = document.createElement('img');
-		galleryItemImage.src = data[i].imageUrl;
-		const galleryItemTitle = document.createElement('figcaption');
+async function getCategories() {
+	const response = await fetch('http://localhost:5678/api/categories');
+	const data = await response.json();
 
-		galleryItemTitle.innerHTML += data[i].title;
+	categories = data;
+
+	displayCategories(categories);
+}
+
+function displayWorks(data) {
+	gallery.innerHTML = '';
+
+	data.forEach((work) => {
+		let galleryItem = document.createElement('figure');
+		let galleryItemImage = document.createElement('img');
+		let galleryItemTitle = document.createElement('figcaption');
+
+		galleryItemImage.src = work.imageUrl;
+		galleryItemTitle.innerHTML = work.title;
 
 		gallery.appendChild(galleryItem);
 		galleryItem.appendChild(galleryItemImage);
 		galleryItem.appendChild(galleryItemTitle);
-	}
+	});
 }
-display(data);
+
+function displayCategories(data) {
+	const filtersAllButton = document.createElement('button');
+	filtersAllButton.textContent = 'Tous';
+	filters.appendChild(filtersAllButton);
+
+	categories.forEach((category) => {
+		let filtersButton = document.createElement('button');
+		filtersButton.textContent = category.name;
+
+		filters.appendChild(filtersButton);
+
+		filtersButton.addEventListener('click', () => {
+			const filteredWorks = works.filter(
+				(work) => work.categoryId === category.id
+			);
+			displayWorks(filteredWorks);
+		});
+	});
+
+	filtersAllButton.addEventListener('click', () => {
+		displayWorks(displayedWorks);
+	});
+}
+
+getCategories();
+getWorks();
